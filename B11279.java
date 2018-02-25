2f2c4f3ef90f1768397c6b671fe30bccd8c3e8d6
alreadyj <kjm71712@naver.com>
2018-02-25 15:46:54 +0900

    solved problem

---
+++ B11279.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+
+public class B11279 {
+    static int q, x;
+    static PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Comparator.reverseOrder());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        q = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (q-- != 0) {
+            x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                Object v = pq.poll();
+                sb.append((v == null) ? 0 : (int)v);sb.append("\n");
+            } else pq.offer(x);
+        }
+        bw.write(sb.toString());
+        bw.flush();bw.close();
+    }
+
+
+}
--- B1351.java
+++ B1351.java
@@ -5,7 +5,7 @@ public class B1351 {
     static int P, Q;
     static long N;
     static String ins[];
-    static HashMap<Long, Integer> dp = new HashMap<>();
+    static HashMap<Long, Long> dp = new HashMap<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -13,16 +13,14 @@ public class B1351 {
         N = Long.parseLong(ins[0]);
         P = Integer.parseInt(ins[1]);
         Q = Integer.parseInt(ins[2]);
-        dp.put(0L, 1);
+        dp.put(0L, 1L);
         bw.write(dp(N) + "\n");
         bw.flush();bw.close();
     }
 
-    public static int dp(long n) {
+    public static long dp(long n) {
         if (dp.containsKey(n)) return dp.get(n);
-        double x = Math.round((double)n / P);
-        double y = Math.round((double)n / Q);
-        dp.put(n, dp((long)x) + dp((long)y));
+        dp.put(n, dp(n / P) + dp(n / Q));
         return dp.get(n);
     }
 }
